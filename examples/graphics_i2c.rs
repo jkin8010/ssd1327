24c94440e3e04bd4be20311d1bd6f58a9c1247af
jkin8010 <jkin8010@gmail.com>
2022-06-15 01:07:10 +0800

    add griphics_i2c example

---
+++ examples/graphics_i2c.rs
@@ -0,0 +1,89 @@
+//! Draw a square, circle and triangle on the screen using the `embedded_graphics` crate.
+//!
+//! This example is for the STM32F103 "Blue Pill" board using I2C1.
+//!
+//! Wiring connections are as follows for a CRIUS-branded display:
+//!
+//! ```
+//!      Display -> Blue Pill
+//! (black)  GND -> GND
+//! (red)    +5V -> VCC
+//! (yellow) SDA -> PB9
+//! (green)  SCL -> PB8
+//! ```
+//!
+//! Run on a Blue Pill with `cargo run --example graphics_i2c`.
+
+use ssd1327::{prelude::*, I2CDisplayInterface, Ssd1327};
+use esp_idf_hal::{self, peripherals::Peripherals, prelude::*};
+use embedded_graphics::{
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
+};
+
+fn main() -> ! {
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio22;
+
+    println!("Starting I2C MSP1503 test");
+
+    let config = <esp_idf_hal::i2c::config::MasterConfig as Default>::default().baudrate(400.kHz().into());
+    let i2c = esp_idf_hal::i2c::Master::new(
+        i2c, 
+        esp_idf_hal::i2c::MasterPins { sda, scl }, 
+        config
+    ).expect("Failed to create I2C master");
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1327::new(interface, DisplaySize128x128, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let yoffset = 50;
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(1)
+        .stroke_color(BinaryColor::On)
+        .build();
+
+    // screen outline
+    // default display size is 128x64 if you don't pass a _DisplaySize_
+    // enum to the _Builder_ struct
+    Rectangle::new(Point::new(0, 0), Size::new(127, 127))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    // triangle
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+    .into_styled(style)
+    .draw(&mut display)
+    .unwrap();
+
+    // square
+    Rectangle::new(Point::new(52, yoffset), Size::new_equal(16))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    // circle
+    Circle::new(Point::new(88, yoffset), 16)
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    loop {}
+}
